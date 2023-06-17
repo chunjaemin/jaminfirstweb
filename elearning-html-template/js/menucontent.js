df34446931d76d9072503392574b1472a49585b8
chunjaemin <chun4582@naver.com>
2023-06-17 18:33:30 +0900

    드디어 성공

--- elearning-html-template/js/menucontent.js
+++ elearning-html-template/js/menucontent.js
@@ -1,17 +1,16 @@
-let scrollY = window.scrollY;
-let scrollX = window.scrollX;
 
-window.addEventListener("scroll", (event) => {
-  let scrollY = this.scrollY;
-  let scrollX = this.scrollX;
-  console.log(scrollY);
-});
 
-function fixedTop(){
-  alert('스크롤 된 위치는 scrollY');
+function locate(){
+  let scrolly = window.scrollY;
+  alert(scrolly);
 }
 
 function visible_click1(){
+  if (menu1.classList.contains('fix1') && !content1.classList.contains('none_visible')){
+    menu1.classList.remove('fix1');
+    content1.classList.remove('basic-setting')
+    window.scrollTo({top:239.382, behavior:'instant'})
+  }
   document.querySelector('.invisible1').classList.toggle('none_visible');
 }
 
@@ -31,12 +30,22 @@ function visible_click5(){
   document.querySelector('.invisible5').classList.toggle('none_visible');
 }
 
-menu1=Document.querySelector('.content-menu1')
-menu2=Document.querySelector('.content-menu2')
-menu3=Document.querySelector('.content-menu3')
+menu1=document.querySelector('.content-menu1');
+menu2=document.querySelector('.content-menu2');
+menu3=document.querySelector('.content-menu3');
 
-menu1ClientRect = menu1.getBoundingClientRect(); // DomRect 구하기 (각종 좌표값이 들어있는 객체)
+content1=document.querySelector('.content-menu-content1');
 
-if (menu1ClientRect.top<=0){
-  
-}
+
+window.addEventListener("scroll", (event) => {
+  content1ClientRect = content1.getBoundingClientRect(); 
+  if (content1ClientRect.top <= 135.5 && !content1.classList.contains('none_visible') && !menu1.classList.contains('fix1')) {
+    menu1.classList.add('fix1');
+    content1.classList.add('basic-setting');
+  } else if (content1ClientRect.top > 135.5 && menu1.classList.contains('fix1')) {
+    menu1.classList.remove('fix1');
+    content1.classList.remove('basic-setting')
+  }
+});
+
+
