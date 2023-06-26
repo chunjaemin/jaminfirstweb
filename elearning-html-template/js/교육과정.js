82af9e4b6695d20ba0c14e2caf51de51852d1a94
chunjaemin <chun4582@naver.com>
2023-06-26 20:50:53 +0000

    저장

--- elearning-html-template/js/교육과정.js
+++ elearning-html-template/js/교육과정.js
@@ -5,9 +5,34 @@ function loadJSON() {
         .then(data => {
             testData = data; // 외부 스코프의 testData 변수에 할당
             console.log(testData);
-            document.getElementById('p1').innerHTML = testData.DATA[0].subj_dvs;
+            var sub1 = [];
+var sub2 = [];
+var sub3 = [];
+var sub4 = [];
+
+testData.DATA.forEach(item => {
+    if (item.subj_grd === "1") {
+        sub1.push(item.subj_dvs);
+    }
+    if (item.subj_grd === "2") {
+        sub2.push(item.subj_dvs);
+    }
+    if (item.subj_grd === "3") {
+        sub3.push(item.subj_dvs);
+    }
+    if (item.subj_grd === "4") {
+        sub4.push(item.subj_dvs);
+    }
+});
+document.getElementById('p1').innerHTML=sub3
+
+console.log(sub1);
+console.log(sub2);
+console.log(sub3);
+console.log(sub4);
         })
         .catch(error => console.error(error));
 }
 
 loadJSON()
+
