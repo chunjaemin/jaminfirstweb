afe4db209da395d882480c97f4c7d8dfaf2bb9e9
chunjaemin <chun4582@naver.com>
2023-07-02 01:06:38 +0900

    저장

--- officer-korean/js/index.js
+++ officer-korean/js/index.js
@@ -1,6 +1,6 @@
 
 var c_inputbox = document.getElementById('c-inputbox')
-var key = document.getElementById('c-select')
+var c_select = document.getElementById('c-select1')
 
 //플래그 변수로 선택
 var flag1 = document.querySelector('.flag1')
@@ -40,6 +40,7 @@ function c_select_search() {
 //사관학교일때
 function military_army(){
     var num=parseInt(c_inputbox.value);
+    var key = parseFloat(c_select.value)
     if (num >= 20040102 && num <= 20080101 && key >= 0){
         flag1.style.setProperty('display','block','important')
     }
@@ -48,6 +49,7 @@ function military_army(){
 //학군사관일때
 function rotc(){
     var num=parseInt(c_inputbox.value);
+    var key = parseFloat(c_select.value)
     if (num >= 19990302 && num <= 20070301 && key >= 1 && key<=2){
         flag2.style.setProperty('display','block','important')
     }
@@ -56,6 +58,7 @@ function rotc(){
 //학사장교-육군일떄
 function b_officer_a(){
     var num=parseInt(c_inputbox.value);
+    var key = parseFloat(c_select.value)
     if (num >= 19990101 && num <= 20070101 && key >= 5){
         flag3.style.setProperty('display','block','important')
     }
@@ -64,6 +67,7 @@ function b_officer_a(){
 //학사장교-공군,해군일때 -올해9월에 지원이 있음 9월이후에 기존년도+1로 수정해주어야함
 function b_officer_b(){
     var num=parseInt(c_inputbox.value);
+    var key = parseFloat(c_select.value)
     if (num >= 19980101 && num <= 20060101 && key >= 5){
         flag3.style.setProperty('display','block','important')
     }
@@ -75,6 +79,7 @@ function b_officer_b(){
 //군가산복무지원 4학년
 function s_officer(){
     var num=parseInt(c_inputbox.value);
+    var key = parseFloat(c_select.value)
     if (key ==1 && num >= 20000702 && num <= 20080702){
         flag4.style.setProperty('display','block','important')
     } 
@@ -93,7 +98,8 @@ function s_officer(){
 
 function pilot(){
     var num=parseInt(c_inputbox.value);
+    var key = parseFloat(c_select.value)
     if (num >= 19740101 && num <= 20040101 && key >= 0){
-        flag3.style.setProperty('display','block','important')
+        flag5.style.setProperty('display','block','important')
     }
 }
