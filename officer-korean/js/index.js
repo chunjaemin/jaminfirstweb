9b8af561207603300d7ce782aa0ca48ee56ef150
chunjaemin <chun4582@naver.com>
2023-07-01 22:37:24 +0900

    저장

--- officer-korean/js/index.js
+++ officer-korean/js/index.js
@@ -8,9 +8,28 @@ function c_select_search() {
     if (c_inputbox.value <19000000){
         alert('생년월일을 올바르게 적어주세요')
     } 
-    //
-    else if (d){
+    //에러가 아닐시 실행
+    else {
 
     }
 }
 
+//사관학교일때
+function military_army(){
+    if (c_inputbox.value >= 200040102 && c_inputbox.value <= 200040102 && c_select.value >= 0){
+        
+    }
+}
+
+//학군사관일때
+
+//학사장교-육군일떄
+
+//학사장교-공군,해군일때 -올해9월에 지원이 있음 9월이후에 기존년도+1로 수정해주어야함
+
+//군가산복무지원 1학년
+//군가산복무지원 2학년
+//군가산복무지원 3학년
+//군가산복무지원 4학년
+
+//육군회전익일때 
