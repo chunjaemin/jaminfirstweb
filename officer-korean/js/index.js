d5778a60619efd40497cbe60a60515ab2850d71c
chunjaemin <chun4582@naver.com>
2023-07-01 22:25:40 +0900

    저장

--- officer-korean/js/index.js
+++ officer-korean/js/index.js
@@ -2,7 +2,15 @@
 var c_inputbox = document.getElementById('c-inputbox')
 var c_select = document.getElementById('c-select')
 
+
 function c_select_search() {
-    //함수들 케이스 적어주면 될 듯
+    //에러캐치
+    if (c_inputbox.value <19000000){
+        alert('생년월일을 올바르게 적어주세요')
+    } 
+    //
+    else if (d){
+
+    }
 }
 
