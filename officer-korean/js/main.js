406e6b7ec95cf77f571e16e7f3af678388e0a175
chunjaemin <chun4582@naver.com>
2023-07-09 16:24:02 +0900

    저장

--- officer-korean/js/main.js
+++ officer-korean/js/main.js
@@ -1,21 +1,4 @@
 
-function visible_click(){
-    var mobilemenu=document.getElementById('n-button')
-    mobilemenu.classList.toggle('none_visible');
-}
-
-function left_fade_function1() {
-    var fadebox1 = document.getElementById('fade-box1')
-    fadebox1.classList.toggle('fixleft')
-}
-
-
-
-function search_computer() {
-    var mini_box = document.getElementById('mini-search-box1');
-    mini_box.classList.toggle('fixleft1');
-}
-
 
 window.onload = function () {
     var menu_hide = document.getElementById('mouseover1');
